c0b309869b00d4f0bbad4fe3d48f377800cc09a4
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-12-22 22:43:48 +0900

    Confirm the callback

--- src/scan_hough_node.cpp
+++ src/scan_hough_node.cpp
@@ -5,6 +5,7 @@ using namespace ros;
 void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
         //on = msg->mid_toggle;
+	ROS_INFO("%f", msg->angle_min);
 }
 
 int main(int argc, char **argv)
@@ -12,4 +13,6 @@ int main(int argc, char **argv)
 	init(argc,argv,"scan_hough");
 	NodeHandle n;
 	Subscriber sub = n.subscribe("/scan", 1, scanCallback);
+
+	ros::spin();
 }
