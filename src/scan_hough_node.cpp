acf1a6ae7022735125a62cc351c60fc3ace45901
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-12-23 13:27:56 +0900

    Rough implementation

--- src/scan_hough_node.cpp
+++ src/scan_hough_node.cpp
@@ -5,7 +5,24 @@ using namespace ros;
 
 class HoughSpace{
 public:
-	short int space[36][10]; //5deg, 10steps of log_e(rho)
+	unsigned short int space[36][20]; //5deg, 10steps of log_e(rho)
+
+	void init(void)
+	{
+		for(int j=0;j<20;j++)
+			for(int i=0;i<36;i++)
+				space[i][j] = 0;
+	}
+
+	void print(void)
+	{
+		for(int j=0;j<20;j++){
+			for(int i=0;i<36;i++){
+				std::cout << space[i][j] << ' ';
+			}
+			std::cout << std::endl;
+		}
+	}
 };
 
 HoughSpace hough;
@@ -19,6 +36,9 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
 	int step = (int)floor( ( msg->angle_max - msg->angle_min ) / msg->angle_increment );
 
+	hough.init();
+	ROS_INFO("START");
+
 	for(int i=0;i<step;i++){
 		if(std::isnan(msg->ranges[i]))
 			continue;
@@ -29,9 +49,16 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 
 		//ROS_INFO("%f %f", x, y);
 		
-		for(i=0;i<36;i++){
+		for(int j=0;j<36;j++){
+			double theta = 3.141592*j*5/180;
+			double rho = x*cos(theta) + y*sin(theta);
+			int log_rho = rho > 0 ? (int)log(rho) + 10 : (int)log(-rho);
+			hough.space[j][log_rho]++;
 		}
 	}
+
+	ROS_INFO("END");
+	hough.print();
 
 }
 
